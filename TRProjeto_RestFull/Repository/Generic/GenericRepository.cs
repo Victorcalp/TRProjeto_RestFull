15f4221ae5c80e1058cfca4d69a183f8f20ae4b7
Victor <victorcalp@outlook.com.br>
2022-12-29 14:34:24 -0300

    Criado Repositorio Generico

---
+++ TRProjeto_RestFull/Repository/Generic/GenericRepository.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Projeto_RestFull.Model.Base;
+using System.Collections.Generic;
+using System.Linq;
+using TRProjeto_RestFull.Model.Context;
+
+namespace TRProjeto_RestFull.Repository.Generic
+{
+    public class GenericRepository<T> : IGeneric<T> where T : BaseEntity
+    {
+        private readonly MYSQLContext _context;
+        private DbSet<T> dbSet;
+
+        public GenericRepository(MYSQLContext context)
+        {
+            _context = context;
+            dbSet = _context.Set<T>();
+        }
+
+        public T Create(T item)
+        {
+            if(item == null) return null;
+            
+            dbSet.Add(item);
+            _context.SaveChanges();
+            return item;   
+        }
+        public T Update(T item)
+        {
+            if (item == null) return null;
+
+            dbSet.Update(item);
+            _context.SaveChanges();
+            return item;
+        }
+
+        public void Delete(long id)
+        {
+            var result = dbSet.SingleOrDefault(x=> x.Id == id);
+            dbSet.Remove(result);
+            _context.SaveChanges();
+        }
+
+        public List<T> FiendAll()
+        {
+            return dbSet.ToList();
+        }
+
+        public T FiendById(long id)
+        {
+            return dbSet.SingleOrDefault(x=> x.Id == id);
+        }
+    }
+}
---
+++ TRProjeto_RestFull/Repository/IGeneric.cs
@@ -0,0 +1,14 @@
+﻿using Projeto_RestFull.Model.Base;
+using System.Collections.Generic;
+
+namespace TRProjeto_RestFull.Repository
+{
+    public interface IGeneric<T> where T : BaseEntity
+    {
+        T Create(T item);
+        T Update(T item);
+        void Delete(long id);
+        T FiendById(long id);
+        List<T> FiendAll();
+    }
+}
