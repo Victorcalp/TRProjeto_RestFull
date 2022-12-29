e2f63d2ccad560c0f0dd8b14df487dd5e9fbe041
Victor <victorcalp@outlook.com.br>
2022-12-29 15:59:51 -0300

    criado cliente business

---
+++ TRProjeto_RestFull/Business/IClienteBusiness.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using TRProjeto_RestFull.Data.VO;
+
+namespace TRProjeto_RestFull.Business
+{
+    public interface IClienteBusiness
+    {
+        ClienteVO Create(ClienteVO item);
+        ClienteVO Update(ClienteVO item);
+        void Delete(long id);
+        ClienteVO FiendById(long id);
+        List<ClienteVO> FiendAll();
+    }
+}
---
+++ TRProjeto_RestFull/Business/Implementation/ClienteBusinessImplementation.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using TRProjeto_RestFull.Data.Converter.Implementation;
+using TRProjeto_RestFull.Data.VO;
+using TRProjeto_RestFull.Model;
+using TRProjeto_RestFull.Repository.Generic;
+
+namespace TRProjeto_RestFull.Business.Implementation
+{
+    public class ClienteBusinessImplementation : IClienteBusiness
+    {
+        private readonly GenericRepository<Cliente> _repository;
+        private readonly ClienteConverter cliente;
+
+        public ClienteBusinessImplementation(GenericRepository<Cliente> repository, ClienteConverter cliente)
+        {
+            _repository = repository;
+            this.cliente = cliente;
+        }
+
+        public ClienteVO Create(ClienteVO item)
+        {
+            var obj = cliente.Parse(item);
+            obj = _repository.Create(obj);
+            return cliente.Parse(obj);
+        }
+
+        public ClienteVO Update(ClienteVO item)
+        {
+            var obj = cliente.Parse(item);
+            obj = _repository.Update(obj);
+            return cliente.Parse(obj);
+        }
+
+        public void Delete(long id)
+        {
+            _repository.Delete(id);
+        }
+
+        public List<ClienteVO> FiendAll()
+        {
+            return cliente.Parse(_repository.FiendAll());
+        }
+
+        public ClienteVO FiendById(long id)
+        {
+            return cliente.Parse(_repository.FiendById(id));
+        }
+    }
+}
